dc1bcba90b3329516382e1652692dbea3cb95ab4
majdevit <majdevit@gmail.com>
2024-10-03 18:59:24 +0300

    release1

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,8 +4,8 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   plugins: [vue()],
   server: {
-    host: '0.0.0.0', // Allow access from outside the container
-    port: 4000, 
+    host: process.env.VITE_HOST || '0.0.0.0', // Explicitly set the host via environment variables
+    port: process.env.VITE_PORT || 4000,      // Set the port from environment variables
   },
   define: {
     '__VUE_PROD_HYDRATION_MISMATCH_DETAILS__': false
